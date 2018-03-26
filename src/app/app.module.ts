14335028ec3fe7e7867abd4c3cdc7283096e8f88
Fourie-r <try.mihaylov@gmail.com>
2018-03-26 17:04:54 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapBoxComponent } from './components/map-box.component';
+import { MapboxService } from './services/mapbox.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapBoxComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [MapboxService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/map-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-box',
+  templateUrl: '../templates/map-box.component.html',
+  styleUrls: ['../styles/map-box.component.css']
+})
+export class MapBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
